eb44ac544c7518ae42c77cd2de2d1ef88f50c441
zmarci1101 <zmarci1101@gmail.com>
2015-05-10 14:44:31 +0200

    controller
    talán kész van

--- controller/CleanerFactory.java
+++ controller/CleanerFactory.java
@@ -1,11 +1,46 @@
 package controller;
+import java.util.List;
+import java.awt.Point;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import view.CleanerView;
+import view.Drawable;
+import view.GamePanel;
+import model.Cleaner;
+
+/**
+ * Cleaner objektumokat létrehozó és törlõ osztály
+ *
+ */
 
 public class CleanerFactory{
 	
+	/**
+	 * létrehoz egy új Cleanert majd visszaadja azt
+	 * @param map
+	 * @param p
+	 * @return
+	 */
+	
 	public static Cleaner create(Map map, Point p){
+		Cleaner cleaner=new Cleaner(map,p);							//létrejön az új Cleaner
+		GamePanel.drawableViews.add(new CleanerView(cleaner));		//létrejön egy CleanerView és bekerül a drawableViews-ba a kirajzolás érdekében
+		Collections.sort(GamePanel.drawableViews);					//rendezi a drawableViews-t
+		return cleaner;												//visszaadja a Cleanert
 	}
 	
-	public static void remove(Cleaner c){
+	/**
+	 * Törli a paraméterben átadott Cleanert
+	 * @param c
+	 */
+	
+	public static void remove(Cleaner c){							
+		for(int i=0;i<GamePanel.drawableViews.size();++i){			//végigmegy a listán és megkeresi az átadott Cleanert
+			if(GamePanel.drawableViews.get(i).equals(c))
+				GamePanel.drawableViews.remove(i);					//ha megtalálta törli
+		}
 	}
 	
 }
--- controller/RobotFactory.java
+++ controller/RobotFactory.java
@@ -1,11 +1,49 @@
 package controller;
 
+import java.awt.Point;
+import java.util.Collections;
+import java.util.Map;
+
+import model.Cleaner;
+import model.Robot;
+import view.CleanerView;
+import view.GamePanel;
+import view.RobotView;
+
+/**
+ * Robot objektumokat létrehozó és törlõ osztály
+ *
+ */
+
 public class RobotFactory {
 	
+	/**
+	 * létrehoz egy új Robotot majd visszaadja azt
+	 *
+	 * @param map
+	 * @param p
+	 * @param v
+	 * @param size
+	 * @return
+	 */
+	
 	public static Robot create(Map map, Point p, Point v, Point size){
+		Robot robot=new Robot(map,p,v);										//létrejön az új Robot
+		GamePanel.drawableViews.add(new RobotView(robot,size));				//létrejön egy RobotView és bekerül a drawableViews-ba a kirajzolás érdekében
+		Collections.sort(GamePanel.drawableViews);							//rendezi a drawableViews-t
+		return robot;														//visszaadja a Robotot
 	}
 	
+	/**
+	 *  Törli a paraméterben átadott Robotot
+	 * @param r
+	 */
+	
 	public static void remove(Robot r){
+		for(int i=0;i<GamePanel.drawableViews.size();++i){					//végigmegy a listán és megkeresi az átadott Robot-ot
+			if(GamePanel.drawableViews.get(i).equals(r))
+				GamePanel.drawableViews.remove(i);							//ha megtalálta törli
+		}
 	}
 	
 }
