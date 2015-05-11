7b5a581a3c2bfd0e4481e758ccfebec6094a2360
zotya701 <zotya701@hotmail.hu>
2015-05-11 03:33:20 +0200

    kis módosítás

--- view/Drawable.java
+++ view/Drawable.java
@@ -2,9 +2,21 @@ package view;
 
 import java.awt.Graphics2D;
 
+/**
+ * A kirajzolható osztályokat fogja össze.
+ */
 public interface Drawable extends Comparable<Drawable>{
 	
+	/**
+	 * Az objektum kirajzolása
+	 * @param g2 A Graphics2D amivel rajzol
+	 */
 	public void Draw(Graphics2D g2);
+	
+	/**
+	 * Ennek segítségével történik a rendezés a kirajzolható objektumokat tároló listában
+	 * @return A prioritás értéke
+	 */
 	public int getPriority();
 	
 }
