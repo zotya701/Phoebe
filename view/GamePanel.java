d34f3dd2d46e55386df78ab9d51a427ba84a5699
zotya701 <zotya701@hotmail.hu>
2015-05-11 03:13:28 +0200

    kis módosítások
    blabla

--- view/GamePanel.java
+++ view/GamePanel.java
@@ -1,16 +1,20 @@
 package view;
-import java.util.*;
+
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JPanel;
 
-import model.GameManager;
+import model.*;
 
 
  /**
@@ -27,33 +31,33 @@ public class GamePanel extends JPanel{
 	public static List<RobotView> robotViews;
 	
 	/**
-	 * Konstruktor, inicializálja a kettõ listát
+	 * Konstruktor, inicializálja a drawableviews
+	 * és robotViews listákat.
 	 */
-	
 	public GamePanel(){
 		if(drawableViews==null) drawableViews = new ArrayList<Drawable>();
 		if(robotViews==null) robotViews = new ArrayList<RobotView>();
-		
 	}
 	
 	/**
-	 * Végig iterál a dravableViews adattagon, és mindegyikre meghívja a draw metódusukat,
+	 * Végig iterál a dravableViews listán, és mindegyikre meghívja a draw metódusukat,
 	 * majd, ha véget ért a játék, akkor ki írja a robotok megtett távolságait.
 	 */
 	public void updateGraphics(){
-		for(int i=0;i< drawableViews.size();i++){
-			drawableViews.get(i).Draw(g2);	
+		g2.setColor(Color.BLACK);											//elõszõr az egészet feketére színezi,
+		g2.fillRect(0, 0, imageOfGame.getWidth(), imageOfGame.getWidth());	//mert külön OutsideFieldView-t nem csináltunk
+		for(int i=0;i<drawableViews.size();i++){
+			drawableViews.get(i).Draw(g2);									//aztán a rendezés szerint sorban kirajzolja az elemeket
 		}
-		
 		if(this.gm.gameEnded()){
 			   this.g2.setFont(new Font("TimesRoman", Font.PLAIN, 30));
 			   g2.setColor(Color.RED);
-			   for(int i=0;i<GameManager.robots.size();++i){
+			   for(int i=0;i<GameManager.robots.size();++i){				//ez itt szépen, formázva jeleníti meg a robotok megtett távolságait
 			    String temp=GameManager.robots.get(i).getId()+". Robot's travelled route:"+GameManager.robots.get(i).getRouteTravelled();
 			          Rectangle2D r = fm.getStringBounds(temp, g2);
 			          int x = (this.imageOfGame.getWidth() - (int) r.getWidth()) / 2;
 			          int y = (this.imageOfGame.getHeight() - (int) r.getHeight()) / (GameManager.robots.size()+1)*(i+1) + fm.getAscent();
-			          g2.drawString(temp, x, y);
+			          g2.drawString(temp, x, y);							//a megfelelõ helyre rajzolja ki a stringet
 			   }
 			  }
 		
@@ -65,20 +69,15 @@ public class GamePanel extends JPanel{
 	 * és kirajzolja mindegyik robot sebességvektorát tartalmazó BufferedImage-et.
 	 */
 	public void paintComponent(Graphics g){
-		g.drawImage(this.imageOfGame,0,0,imageOfGame.getWidth(), imageOfGame.getHeight(), null);
-		
-		
-		if(!this.gm.gameEnded()){
-			for(int i=0; i<robotViews.size();i++){
-				g.drawImage(robotViews.get(i).getImgForVelVec(),0, 0, gm.getMapSize().x*25, gm.getMapSize().y*25, null);
-				
+		super.paintComponent(g);
+		if(gm!=null){
+			g.drawImage(imageOfGame, 0, 0, imageOfGame.getWidth(), imageOfGame.getHeight(), null);	//célszerûbb csak egy képet kirajzolni, és a képre rajzolni amikor szükséges 
+			if(!gm.gameEnded()){
+				for(RobotView rv : robotViews){
+					g.drawImage(rv.getImgForVelVec(), 0, 0, gm.getMapSize().x*25, gm.getMapSize().y*25, null);
+				}
 			}
-			
-			
-			
-			
 		}
-		
 	}
 	
 /**
@@ -89,8 +88,12 @@ public class GamePanel extends JPanel{
  */
 	public void init(GameManager gm){
 		this.gm=gm;
-		imageOfGame = new BufferedImage(gm.getMapSize().x*25, gm.getMapSize().y*25, BufferedImage.TYPE_INT_ARGB);		
-		g2= imageOfGame.createGraphics();
+		this.imageOfGame=new BufferedImage(gm.getMapSize().x*25, gm.getMapSize().y*25, BufferedImage.TYPE_INT_ARGB);
+		this.g2=this.imageOfGame.createGraphics();
+		this.g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		this.setPreferredSize(new Dimension(gm.getMapSize().x*25, gm.getMapSize().y*25));
+		this.g2.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		this.fm = g2.getFontMetrics(new Font("TimesRoman", Font.PLAIN, 30));
 	}
 	
 }
